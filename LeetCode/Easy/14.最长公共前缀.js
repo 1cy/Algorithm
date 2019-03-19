da59dd9ff3350ac3a47756ced1f3e3ebf9b01b09
lengchy <lengchy@yonyou.com>
2019-03-19 15:38:00 +0800

    first commit

---
+++ LeetCode/Easy/14.最长公共前缀.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=javascript
+ *
+ * [14] 最长公共前缀
+ *
+ * https://leetcode-cn.com/problems/longest-common-prefix/description/
+ *
+ * algorithms
+ * Easy (32.24%)
+ * Total Accepted:    57K
+ * Total Submissions: 176.8K
+ * Testcase Example:  '["flower","flow","flight"]'
+ *
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * 
+ * 示例 1:
+ * 
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * 
+ * 
+ * 说明:
+ * 
+ * 所有输入只包含小写字母 a-z 。
+ * 
+ */
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(strs.length == 0) return '';
+    return strs.reduce( (total, current) => {
+        let length = total.length;
+        for(let i= 0; i< length; i++){
+            if( total[i] != current[i]){
+                return total.slice(0, i);
+            }
+        }
+        return total;
+    }, strs[0]);
+};
+
